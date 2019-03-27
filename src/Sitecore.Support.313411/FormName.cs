0868f114ce547db7dd5763104e66635f4d3aa328
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2019-03-27 17:02:15 +0200

    Use additional computed field for indexing untokenized Sitecore forms names in lowercase and sort by this field

---
+++ src/Sitecore.Support.313411/FormName.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.Data.Items;
+using Sitecore.ExperienceForms.Mvc.Constants;
+
+namespace Sitecore.Support.ContentSearch.ComputedFields
+{
+  public class FormName : AbstractComputedIndexField
+  {
+    public override object ComputeFieldValue(IIndexable indexable)
+    {
+      Item item = indexable as SitecoreIndexableItem;
+      if (item != null && item.TemplateID == TemplateIds.FormTemplateId)
+      {
+        return item.Name.ToLower();
+      }
+      return null;
+    }
+  }
+}
---
+++ src/Sitecore.Support.313411/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.313411")]
+[assembly: AssemblyProduct("Sitecore.Support.313411")]
+[assembly: ComVisible(false)]
